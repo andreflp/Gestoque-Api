d3aead605840aaabcaf93cdbbdc123348f02638b
andre <flpandresilva@gmail.com>
2019-01-23 00:14:16 -0200

    Fix routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,6 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = fs
-  .readdirSync('../../Dev/gestoque-api/src/routes')
+  .readdirSync(__dirname)
   .filter(file => file.indexOf('.') !== 0 && file !== 'index.js')
   .forEach(file => require(path.resolve(__dirname, file)))
